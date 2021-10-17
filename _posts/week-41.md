84660ece98636d80dd1ed5b906f3aa952e4fef1d
Josh Unger <joshunger1@gmail.com>
2021-10-17 08:31:11 -0600

    try out week 41

---
+++ _posts/week-41.md
@@ -0,0 +1,31 @@
+---
+title: "Week 41"
+excerpt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. At imperdiet dui accumsan sit amet nulla facilities morbi tempus."
+# coverImage: "/assets/blog/dynamic-routing/cover.jpg"
+date: "2021-10-17"
+# author:
+#   name: Josh Unger
+# picture: "/assets/blog/authors/jj.jpeg"
+ogImage:
+  url: "/assets/blog/dynamic-routing/cover.jpg"
+---
+
+## What I'm thinking about
+
+## What I'm listening to
+
+## What I'm reading
+
+## What I'm watching
+
+- Deadpool 2016
+- Buried
+- Deadpool 2
+- Adventureland
+- Free Guy
+
+## Quote I'm pondering
+
+> Say thank you in advance for what is already yours.
+
+&mdash; <cite>Denzel Washington</cite>
--- components/hero-post.js
+++ components/hero-post.js
@@ -1,7 +1,7 @@
-import Avatar from '../components/avatar'
-import DateFormatter from '../components/date-formatter'
-import CoverImage from '../components/cover-image'
-import Link from 'next/link'
+import Avatar from "../components/avatar";
+import DateFormatter from "../components/date-formatter";
+import CoverImage from "../components/cover-image";
+import Link from "next/link";
 
 export default function HeroPost({
   title,
@@ -13,15 +13,17 @@ export default function HeroPost({
 }) {
   return (
     <section>
-      <div className="mb-8 md:mb-16">
-        <CoverImage
-          title={title}
-          src={coverImage}
-          slug={slug}
-          height={620}
-          width={1240}
-        />
-      </div>
+      {coverImage && (
+        <div className="mb-8 md:mb-16">
+          <CoverImage
+            title={title}
+            src={coverImage}
+            slug={slug}
+            height={620}
+            width={1240}
+          />
+        </div>
+      )}
       <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
         <div>
           <h3 className="mb-4 text-4xl lg:text-6xl leading-tight">
@@ -35,9 +37,9 @@ export default function HeroPost({
         </div>
         <div>
           <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
-          <Avatar name={author.name} picture={author.picture} />
+          {author && <Avatar name={author.name} picture={author.picture} />}
         </div>
       </div>
     </section>
-  )
+  );
 }
