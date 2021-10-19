df5f53de9188cb2aa92c5aba1aa0d1f0662eb680
Josh Unger <joshunger1@gmail.com>
2021-10-19 09:22:44 -0600

    remove alert and meta

--- components/layout.js
+++ components/layout.js
@@ -1,16 +1,16 @@
-import Alert from '../components/alert'
-import Footer from '../components/footer'
-import Meta from '../components/meta'
+import Alert from "../components/alert";
+import Footer from "../components/footer";
+import Meta from "../components/meta";
 
 export default function Layout({ preview, children }) {
   return (
     <>
       <Meta />
       <div className="min-h-screen">
-        <Alert preview={preview} />
+        {/* <Alert preview={preview} /> */}
         <main>{children}</main>
       </div>
       <Footer />
     </>
-  )
+  );
 }
--- components/meta.js
+++ components/meta.js
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import { CMS_NAME, HOME_OG_IMAGE_URL } from '../lib/constants'
+import Head from "next/head";
+import { CMS_NAME, HOME_OG_IMAGE_URL } from "../lib/constants";
 
 export default function Meta() {
   return (
@@ -32,11 +32,11 @@ export default function Meta() {
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
       <meta name="theme-color" content="#000" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-      <meta
+      {/* <meta
         name="description"
         content={`A statically generated blog example using Next.js and ${CMS_NAME}.`}
-      />
+      /> */}
       <meta property="og:image" content={HOME_OG_IMAGE_URL} />
     </Head>
-  )
+  );
 }
