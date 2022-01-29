b9890db81b404b8af64205dd07a40dc8dc806b40
Josh Unger <joshunger1@gmail.com>
2022-01-29 07:53:26 -0700

    .

--- components/post-preview.js
+++ components/post-preview.js
@@ -1,7 +1,7 @@
-import Avatar from '../components/avatar'
-import DateFormatter from '../components/date-formatter'
-import CoverImage from './cover-image'
-import Link from 'next/link'
+import Avatar from "../components/avatar";
+import DateFormatter from "../components/date-formatter";
+import CoverImage from "./cover-image";
+import Link from "next/link";
 
 export default function PostPreview({
   title,
@@ -13,15 +13,17 @@ export default function PostPreview({
 }) {
   return (
     <div>
-      <div className="mb-5">
-        <CoverImage
-          slug={slug}
-          title={title}
-          src={coverImage}
-          height={278}
-          width={556}
-        />
-      </div>
+      {coverImage && (
+        <div className="mb-5">
+          <CoverImage
+            slug={slug}
+            title={title}
+            src={coverImage}
+            height={278}
+            width={556}
+          />
+        </div>
+      )}
       <h3 className="text-3xl mb-3 leading-snug">
         <Link as={`/posts/${slug}`} href="/posts/[slug]">
           <a className="hover:underline">{title}</a>
@@ -31,7 +33,7 @@ export default function PostPreview({
         <DateFormatter dateString={date} />
       </div>
       <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
-      <Avatar name={author.name} picture={author.picture} />
+      {author && <Avatar name={author.name} picture={author.picture} />}
     </div>
-  )
+  );
 }
