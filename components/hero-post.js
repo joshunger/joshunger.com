4c37d15e29c277094022f919056d716669c91b49
Josh Unger <joshunger1@gmail.com>
2022-02-19 07:46:30 -0700

    null check date

--- components/hero-post.js
+++ components/hero-post.js
@@ -1,7 +1,7 @@
-import Avatar from "../components/avatar";
-import DateFormatter from "../components/date-formatter";
-import CoverImage from "../components/cover-image";
-import Link from "next/link";
+import Avatar from '../components/avatar';
+import DateFormatter from '../components/date-formatter';
+import CoverImage from '../components/cover-image';
+import Link from 'next/link';
 
 export default function HeroPost({
   title,
@@ -31,9 +31,11 @@ export default function HeroPost({
               <a className="hover:underline">{title}</a>
             </Link>
           </h3>
-          <div className="mb-4 md:mb-0 text-lg">
-            <DateFormatter dateString={date} />
-          </div>
+          {date && (
+            <div className="mb-4 md:mb-0 text-lg">
+              <DateFormatter dateString={date} />
+            </div>
+          )}
         </div>
         <div>
           <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
