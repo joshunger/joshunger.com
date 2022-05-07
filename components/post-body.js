afb485d4108d320a752dfdaaaa83da7fabf20b19
Josh Unger <joshunger1@gmail.com>
2022-05-07 07:48:51 -0600

    4xl

--- components/post-body.js
+++ components/post-body.js
@@ -1,12 +1,12 @@
-import markdownStyles from './markdown-styles.module.css'
+import markdownStyles from './markdown-styles.module.css';
 
 export default function PostBody({ content }) {
   return (
-    <div className="max-w-3xl mx-auto">
+    <div className="max-w-4xl mx-auto">
       <div
         className={markdownStyles['markdown']}
         dangerouslySetInnerHTML={{ __html: content }}
       />
     </div>
-  )
+  );
 }
